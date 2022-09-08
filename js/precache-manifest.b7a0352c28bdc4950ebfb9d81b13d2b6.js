e9c64c7a666b20a60aaa6f4babda23265f82b7c9
eceiruuuu <79126669+759108118@users.noreply.github.com>
2022-09-08 14:13:31 +0800

    Add files via upload

---
+++ js/precache-manifest.b7a0352c28bdc4950ebfb9d81b13d2b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe56fcae5ba4d31ef28",
+    "url": "css/app.css"
+  },
+  {
+    "revision": "e4c9d19088bfd0d3b82ac5c3897c29a2",
+    "url": "index.html"
+  },
+  {
+    "revision": "0fe56fcae5ba4d31ef28",
+    "url": "js/app.js"
+  },
+  {
+    "revision": "aab8accbc00f80e178f0",
+    "url": "js/chunk-vendors.js"
+  },
+  {
+    "revision": "076251330278aa7c04233c7845d704fd",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
